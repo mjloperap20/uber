738c26a2f0662ef0b9791f3acaeb3f7d390c3954
mjloperap20 <mlopera@sura.com.co>
2021-12-06 06:08:34 -0500

    listar, crear y editar rutas

--- src/app/modulos/rutas/edit/edit.component.ts
+++ src/app/modulos/rutas/edit/edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RutaModelo } from 'src/app/modelos/ruta.model';
+import { RutasService } from 'src/app/servicios/rutas.service';
+import Swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +13,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private rutasService: RutasService,
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+    fgValidacion = this.fb.group({
+      id: ['', [Validators.required]],
+      tiempo_estimado: ['', [Validators.required]],
+      origen: ['', [Validators.required]],
+      destino: ['', [Validators.required]],
+    });
+ 
+    id: string=''
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params["id"]
+    this.buscarRegistro(this.id);
   }
+
+  buscarRegistro(id: string){
+    this.rutasService.getWithId(id).subscribe((data: RutaModelo) => {
+      console.log(data)
+      this.fgValidacion.controls["id"].setValue(id)
+      this.fgValidacion.controls["tiempo_estimado"].setValue(data.tiempo_estimado)
+      this.fgValidacion.controls["origen"].setValue(data.origen)
+      this.fgValidacion.controls["destino"].setValue(data.destino)
+    })
+  }
+
+  edit(){
+    let ruta = new RutaModelo();
+    ruta.id = this.fgValidacion.controls["id"].value;
+    ruta.tiempo_estimado = this.fgValidacion.controls["tiempo_estimado"].value;
+    ruta.origen = this.fgValidacion.controls["origen"].value;
+    ruta.destino = this.fgValidacion.controls["destino"].value;
+ 
+ 
+    this.rutasService.update(ruta).subscribe((data: RutaModelo)=> {
+      Swal.fire('Editado Correctamente!', '', 'success')
+      this.router.navigate(['/rutas/get']);
+    },
+    (error: any) => {
+      console.log(error)
+      alert("Error en el envio");
+    })
+  }
+
+
 
 }
